d7296351b20e2987cb0be267573b0a761f6b3936
Marc Majcher <majcher@gmail.com>
2017-07-11 16:28:07 -0500

    stubs

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,25 @@
 
 const express = require('express');
 const router = express.Router();
+const theaters = require('../lib/theaters');
 
 router.get('/', (req, res) => {
   res.send({
     api: 'ok'
   });
+});
+
+router.get('/hideout', (req, res) => {
+  res.send(theaters.hideout(new Date()));
+});
+router.get('/tit', (req, res) => {
+  res.send(theaters.tit(new Date()));
+});
+router.get('/tnm', (req, res) => {
+  res.send(theaters.tnm(new Date()));
+});
+router.get('/ctt', (req, res) => {
+  res.send(theaters.ctt(new Date()));
 });
 
 module.exports = router;
